5fc7b6e3823dac5bab1e0b0d02ed6199b5312766
tdiciaccio <tknack66@gmail.com>
2018-07-19 10:37:05 +0200

    Add comments

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,12 +5,13 @@ import { Notes } from './notes'
 
 if (Meteor.isServer){
   const userId = 'testUserId1';
-  describe('notes', function () {
+  const noteId = 'testNoteId1';
 
+  describe('notes', function () {
     beforeEach(function () {
       Notes.remove({});
       Notes.insert({
-        _id: 'testNoteId1',
+        _id: noteId,
         title: 'My Title',
         body: 'My body for note',
         updateAt: 0,
@@ -22,7 +23,7 @@ if (Meteor.isServer){
       // const userId = 'testid';
       const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId })
 
-      expect(Notes.findOne({ _id, userId })).toBeTruthy();
+      expect(Notes.findOne({ _id, userId })).toBeTruthy(); // toExist
     })
 
     it('should not insert note if not authenticated', function () {
@@ -33,21 +34,21 @@ if (Meteor.isServer){
 
     it('should remove note', function () {
       // const userId = 'testUserId1';
-      Meteor.server.method_handlers['notes.remove'].apply({ userId }, ['testNoteId1']);
+      Meteor.server.method_handlers['notes.remove'].apply({ userId }, [noteId]); // remove correct
 
-      expect(Notes.findOne({_id: 'testNoteId1'})).toBeFalsy();
+      expect(Notes.findOne({_id: noteId})).toBeFalsy(); // remplace toNotExist
     })
 
     it('should not remove note if unauthenticated', function () {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId1'])
+        Meteor.server.method_handlers['notes.remove'].apply({}, [noteId]) // pas de userId
       }).toThrow()
     })
 
     it('should not remove note if invalid _id', function () {
       // const userId = 'testUserId1';
       expect (() => {
-        Meteor.server.method_handlers['notes.remove'].apply({ userId }, [])
+        Meteor.server.method_handlers['notes.remove'].apply({ userId }, []) // pas de noteId
       }).toThrow();
     })
 
